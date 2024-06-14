19ad96701121e752dffbf599622245bf5dd8da36
Giu1447 <giulian.mazzeo@lernende.bbw.ch>
2024-06-14 10:38:10 +0200

    Implemented PATCH /tasks/{id} Endpoint

--- LB/endpoints/tasks.js
+++ LB/endpoints/tasks.js
@@ -45,9 +45,13 @@ router.post('/', (req, res) => {
 
         req.body.id = newId
 
-        tasksList.push(newTask)
-        fs.writeFileSync(taskPath, JSON.stringify(tasksList, null, 2))
-        res.status(201).json(tasksList[newId - 1])
+        if (!newTask.title || !newTask.description || !newTask.dueDate || newTask.resolvedDate) {
+            res.status(422).send('Sie müssen alles aktualisieren')
+        } else {
+            tasksList.push(newTask)
+            fs.writeFileSync(taskPath, JSON.stringify(tasksList, null, 2))
+            res.status(201).json(tasksList[newId - 1])
+        }
     } catch (err) {
         console.error('Fehler beim Hinzufügen eines Tasks:', err)
         res.status(500).send('Das hat nicht funktioniert')
@@ -114,6 +118,54 @@ router.put('/:id', (req, res) => {
         res.status(200).json(tasksList[updateIndex])
     } catch (error) {
         console.error('Fehler beim Aktualisieren eines Tasks:', error)
+        res.status(500).send('Ein Fehler ist aufgetreten')
+    }
+})
+
+
+router.patch('/:id', (req, res) => {
+    /*
+    #swagger.tags = ['Tasks']
+    #swagger.parameters['body'] = {
+    in: 'body',
+    description: 'Details zum Task um ihn teilweise zu aktualisieren.',
+    required: true,
+    schema: {
+        type: 'object',
+        properties: {
+        "title": { type: 'string', description: 'Updated Title (optional)' },
+        "description": { type: 'string', description: 'Updated description (optional)' },
+        "dueDate": { type: 'string', description: 'Updated dueDate (optional)' },
+        "resolvedDate": { type: 'string', description: 'Updated resolvedDate (optional)' }
+    }
+  }
+}
+*/
+    try {
+        const updateid = req.params.id
+        const newupdateTask = req.body
+        const updateTask = tasksList.find(task => task.id === parseInt(updateid))
+
+        if (!updateTask) {
+            return res.status(404).send('Buch nicht gefunden')
+        }
+
+        if (newupdateTask.title) {
+            updateTask.title = newupdateTask.title
+        }
+
+        if (newupdateTask.author) {
+            updateTask.author = newupdateTask.author
+        }
+
+        if (newupdateTask.year) {
+            updateTask.year = newupdateTask.year
+        }
+
+        fs.writeFileSync(taskPath, JSON.stringify(tasksList, null, 2))
+        res.status(200).json(updateTask)
+    } catch (error) {
+        console.error('Fehler beim Aktualisieren des Tasks:', error)
         res.status(500).send('Ein Fehler ist aufgetreten')
     }
 })
